7daf83ee736343af47b057c995f980c4961e9b13
Utsav Anand <utsavanand2@gmail.com>
2019-04-29 11:37:27 +0530

    add prom-client

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require('http')
+const Prometheus = require('prom-client')
 
 const requestLogs = []
 const PORT = process.env.PORT || 8000
 
 const server = http.createServer((req, res) => {
-    requestLogs.push({ url: req.url, date: new Date() })
-    res.end(JSON.stringify(requestLogs))
+    if (req.url == '/') {
+        requestLogs.push({ url: req.url, date: new Date() })
+        res.end(JSON.stringify(requestLogs))
+    } else if (req.url == '/metrics') {
+        res.setHeader('Content-Type', Prometheus.register.contentType)
+        res.end(Prome)
+    }
+    
 })
 
 server.listen(PORT)
