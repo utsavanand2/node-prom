8f080480789ac83239b537fd7f826b7504dff2a0
Utsav Anand <utsavanand2@gmail.com>
2019-04-29 10:39:21 +0530

    change the exposed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 
 const filename = './requests.json'
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
 
 const readRequests = () => {
     try {
